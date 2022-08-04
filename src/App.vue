b882cf36ec224d03dfbd6a44a849e6e30810ff4f
Leithen Crider <leithen@mini.local>
2022-08-03 18:58:23 -0600

    Creating intial maze state framework

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,26 @@
     import Generator from './components/Generator.vue';
 </script>
 
+<script>
+    export default {
+        // state
+        data() {
+            return {
+                mazeArr: []
+            }
+        },
+        // actions
+        methods: {
+            fill(arr) {
+                this.mazeArr = arr;
+            },
+            isEmpty() {
+                this.mazeArr.length > 0 ? false : true;
+            }
+        }
+    }
+</script>
+
 <template>
     <!-- <Maze :length="15" :width="15"/> these values determine the size of the maze  -->
     <TestMaze/>
