dcc891544fc22e2f340b6dc0a6aa80b2033508d2
Thenlie <leithen113@gmail.com>
2022-08-19 14:46:49 -0600

    Update MazeCell class

--- src/lib/MazeCell.js
+++ src/lib/MazeCell.js
@@ -1,16 +1,15 @@
 class MazeCell {
     constructor(x, y, visited, top, right, bottom, left) {
-        this.x = x; // x coordinate location
-        this.y = y; // y coordinate location
-        this.visited = visited; // checked in search, defaults to False
-        /*  -- true if wall, false if air --  */
-        this.top = top; 
-        this.right = right; 
-        this.bottom = bottom; 
-        this.left = left;
+        this.x = x; // x coordinate location -- INT
+        this.y = y; // y coordinate location -- INT
+        this.visited = visited; // checked in search, defaults to False -- BOOL
+        this.top = top; // true if wall, false if air --  BOOL
+        this.right = right; // ^
+        this.bottom = bottom; // ^
+        this.left = left; // ^
     }
     getLocation() {
-        return { x: this.x, y: this.y }
+        return { x: this.x, y: this.y };
     }
     wasVisited() {
         return this.visited;
