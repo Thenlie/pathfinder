49506267c1e9f7ffd553d22b8091ba49af94eb4e
Thenlie <leithen113@gmail.com>
2022-08-19 21:30:36 -0600

    Removed unused methods on MazeCell class

--- src/lib/MazeCell.js
+++ src/lib/MazeCell.js
@@ -8,24 +8,6 @@ class MazeCell {
         this.bottom = bottom; // ^
         this.left = left; // ^
     }
-    getLocation() {
-        return { x: this.x, y: this.y };
-    }
-    wasVisited() {
-        return this.visited;
-    }
-    hasTop() {
-        return this.top;
-    }
-    hasRight() {
-        return this.right;
-    }
-    hasBottom() {
-        return this.bottom;
-    }
-    hasLeft() {
-        return this.left;
-    }
 }
 
 export { MazeCell };
