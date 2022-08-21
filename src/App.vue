25d8bc29e51426c71726212dead9ab99705263a7
Thenlie <leithen113@gmail.com>
2022-08-21 12:50:11 -0600

    Cleaning up App.vue, moved components to Home.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <script setup>
     import TestMaze from './components/TestMaze.vue';
     import Generator from './components/Generator.vue';
-    import NewGenerator from './components/NewGenerator.vue';
 </script>
 
 <template>
-    <TestMaze />
-    <!-- <Generator :length="15" :width="15" :mazeArr="mazeArr" @set="set" /> -->
-    <NewGenerator :length="50" :width="50" :mazeArr="mazeArr" @set="set" /> <!-- these values determine the size of the maze  -->
     <router-view></router-view>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
+<script setup>
+    import NewGenerator from '../components/NewGenerator.vue';
+</script>
+
 <template>
     <div class="home">
         <h1>Home</h1>
+        <NewGenerator :length="50" :width="50" :mazeArr="mazeArr" @set="set" /> <!-- these values determine the size of the maze  -->
     </div>
 </template>
