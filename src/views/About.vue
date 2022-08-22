c62a4c115e3b52f0a9f60c2bdf34a08358fd7c85
Thenlie <leithen113@gmail.com>
2022-08-21 21:27:49 -0600

    Significant additional to About section

--- src/views/About.vue
+++ src/views/About.vue
@@ -9,19 +9,81 @@
         <div class="dot-container">
             <Dots />
         </div>
-        <h2>🚧 Page Under Construction 🚧</h2>
-        <p>Pathfinder is a website built to demonstrate pathfinding algorithms! This project was started so the developers could learn more about these algorithms and have a fun project to work on in their spare time. Started in July 2022, this was also the developers first time seeing, learning and using Vue.js</p>
+        <p>Pathfinder is a website built to demonstrate pathfinding algorithms! This project was started so the developers could learn more about these algorithms and have a fun project to work on in their spare time. Started in July 2022, this was also the developers first time seeing, learning, and using Vue.js</p>
+        <p>This project ended up being a great learning experience for all involved. It not only taught logical and algorithmic thinking, but it also taught skills on collaboration and team software development. Many tools were used to assist in the development. Some key tools include; Git, GitHub, VS Cdoe, Notion, and Discord.</p>
         <h2>Maze Generation</h2>
         <p>Unbeknownst to the developers at first, the first step in building a really good pathfinding algorithim, is building a really good maze generator. Without lots of automated testing, it is really hard to know how error proof your code is. Additionally, hardcoding mazes gets really tedious after just one or two. This leads to the inevitability of creating a maze generation algorithm!</p>
+        <h3>What does a maze look like in code?</h3>
+        <p>To represent a maze in code, we start with a two dimentional array. This is nothing more than an array of arrays. Below is an example of a 10 x 10 2D array and how to generate one with nested for loops:</p>
+        <div class="code-container">
+            <highlightjs language="javascript" class="code" code="
+const sampleMaze = [
+    ['S', '0', '0', '#', '0', '0', '0', '#', '0', '0'],
+    ['#', '0', '#', '0', '0', '#', '0', '#', '#', '0'],
+    ['#', '0', '0', '0', '#', '#', '0', '0', '0', '0'],
+    ['0', '0', '0', '#', '0', '0', '0', '#', '0', '0'],
+    ['0', '0', '0', '#', '0', '0', '#', '#', '0', '#'],
+    ['#', '0', '#', '0', '#', '0', '0', '0', '#', '0'],
+    ['0', '#', '#', '0', '0', '#', '#', '0', '#', '0'],
+    ['0', '0', '0', '0', '0', '0', '0', '0', '#', '0'],
+    ['0', '0', '0', '0', '#', '#', '#', '#', '#', '0'],
+    ['0', '#', '0', '0', '0', '0', '0', '0', '0', 'F'],
+];" 
+            />
+            <highlightjs language="javascript" class="code" code="
+function generateArray() {
+    let arr2D = [];
+    for (let i = 0; i < length; i++) {
+        arr2D.push([]);
+    }
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < width; j++) {
+            if (arr2D[i]) {
+                arr2D[i].push(0))
+            } 
+        }
+    }
+    return arr2D
+}"
+            />
+        </div>
+        <p>In this example, you can see our first idea for representing the maze. We would simply use characters to represent each state of the maze cells. A '#' would represent a wall, 'S' and 'F' would be start and finish respectively, and '0' would represent and empty cell. We could also use '@' to notate cells we have already traveled to. To access individual cells of the maze we target the vertical and then horizontal positon in the array. So the first cell would be 'arr2D[0][0]'</p>
+        <p></p>
         <h3>Attempt #1</h3>
-        <p>For the frist attempt, the thought was that we needed to prioritize having a path to the exit. If we were to just randomly generate walls, there is a change the start would be completely closed off, which we would consider an invalid maze. So, to ensure we always generated a valid maze, the plan was to first generate a random path to the exit. </p>
+        <p>For the frist attempt, the thought was that we needed to prioritize having a path to the exit. If we were to just randomly generate walls, there is a change the start would be completely closed off, which we would consider an invalid maze. So, to ensure we always generated a valid maze, the plan was to first generate a random path to the exit. Once that is completed, walls can be filled in.</p>
+        <p>The path was to be created by moving a node around a grid and marking each cell as part of the path. The idea was that each new node would touch exactly one other node on the path. This would ensure the path did not loop upon itself. To do this, a function was created to check the current nodes surrounding positions. This involved one function that returned the value of the location being checked, and another function that combined all of the individual values to see where valid moves were</p>
+        <div class="code-container">
+            <highlightjs language="javascript" class="code" code="
+    const checkPosition = (arr2D, x, y) => {
+        // return value of (x, y) in 2D array
+        if (arr2D[x]) {
+            if (arr2D[x][y] === 0) {
+                return 0;
+            } else if (arr2D[x][y] === '@') {
+                return '@';
+            }
+        } 
+        return null;
+    };"/>
+            <highlightjs language="javascript" class="code" code="
+    const checkSurroundings = (arr2D, x, y) => {
+        // check for available directions to move
+        let arr = [];
+        if (checkPosition(arr2D, x-1, y)) { arr.push('U') };
+        if (checkPosition(arr2D, x+1, y)) { arr.push('D') };
+        if (checkPosition(arr2D, x, y-1)) { arr.push('L') };
+        if (checkPosition(arr2D, x, y+1)) { arr.push('R') };
+        return arr;
+    };"/>
+        </div>
+        <p>The 'checkSurroundings()' function is actually significantly shortened from the original. Instead of being able to check only 4 locations, as shown here, 12 locations had to be checked. This is because we needed to know the values of every cell touching the current cell, AND every cell touching those cells.</p>
         <h3>Attempt #2</h3>
-        <p></p>        <Generator :length="10" :width="10" :mazeArr="mazeArr" :page="2" @set="set" /> <!-- these values determine the size of the maze  -->
+        <p></p>        
+        <Generator :length="10" :width="10" :mazeArr="mazeArr" :page="2" @set="set" /> <!-- these values determine the size of the maze  -->
     </main>
 </template>
 
 <script>
-    import hljs from 'highlight.js/lib/common';
     import hljsVuePlugin from "@highlightjs/vue-plugin";
 
     export default {
@@ -45,13 +107,25 @@
 </script>
 
 <style scoped>
-    h2 {
+    h2, h3 {
         text-align: center;
         margin: 1em;
     }
     p {
-        text-align: center;
+        text-indent: 30px;
+        text-align:justify;
         font-size: large;
         margin: 0.5em;
+    }
+    .code {
+        width: fit-content;
+        margin: auto;
+        display: inline;
+    }
+
+    .code-container {
+        background-color: #f6f6f6;
+        display: flex;
+        flex-wrap: wrap;
     }
 </style>
