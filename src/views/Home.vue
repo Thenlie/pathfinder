c585448fbecfb1c967e8010d467547ae82e99c31
Thenlie <leithen113@gmail.com>
2022-08-30 19:27:36 -0600

    Added front end inputs to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,6 +12,13 @@
         <div class="dot-container">
             <Dots />
         </div>
+        <div class="input-container">
+            <label for="length">Length: </label>
+            <input name="length" />
+            <label for="width">Width: </label>
+            <input name="width" />
+            <button>Apply</button>
+        </div>
         <div class="btn-container">
             <Generator :length="25" :width="25" :mazeArr="mazeArray.array" :page="1" /> <!-- these values determine the size of the maze -->
             <Solver :page="1" />
@@ -25,9 +32,12 @@
         text-align: center;
         font-size: 48px;
     }
-    .btn-container {
+    .btn-container, .input-container {
         display: flex;
         justify-content: center;
         align-items: center;
+    }
+    input {
+        margin: 0.5em;
     }
 </style>
